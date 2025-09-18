40c7bba7ec653a898b0943461ae59c9212e769cd
HerniMaryani <hernimaryani_2428250017@mhs.mdp.ac.id>
2025-09-18 18:42:01 +0700

    login & register

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -5,17 +5,40 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthenticationController extends Controller
 {
     public function login(LoginRequest $request)
     {
         try {
+            $validated = $request->safe()->all();
+            if(!Auth::attempt($validated)){
+                  return response()->json([
+                    'message' => 'Email atau password salah',
+                    'data' => null
+                ], 401);
+            }
+
+            $user = $request->user();
+            $token = $user->createToken('laravel_api', ['*'])->plainTextToken;
+            
+            return response()->json([
+                'message' => 'Login Berhasil',
+                'user' => $user,
+                'token' => $token
+            ], 200);
         
         } catch (Exception $e) {
-            
+              return response()->json([
+                    'message' => $e->getMessage(),
+                    'data' => null
+                ], 500);
         }
     }
 
@@ -23,8 +46,24 @@ class AuthenticationController extends Controller
     {
         try {
             $validated = $request->safe()->all();
+
+            $passwordHash = Hash::make($validated['password']);
+
+            $validated['password']=$passwordHash;
+
+            $response = User::create($validated);
+
+            if($response){
+                return response()->json([
+                    'message' => 'register berhasil',
+                    'data' => null
+                ], 201);
+            }
         } catch (Exception $e) {
-            //
+            return response()->json([
+                    'message' => $e->getMessage(),
+                    'data' => null
+                ], 500);
         }
     }
 
--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class RegisterRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return Auth::guest();
     }
 
     /**
@@ -22,7 +23,9 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed'
         ];
     }
 }
